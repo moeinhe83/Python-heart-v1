2c31c747e758c65feafd832fd96d1a47a91eb497
Moein Heshmati <moeinheshmati1383@gmail.com>
2024-02-16 14:24:28 +0330

    Upload Python-heart-v1 project on Github

---
+++ README.md
@@ -0,0 +1,13 @@
+Project Name ===> Make Heart With Python
+
+Program Name ===> Heart Version 1
+
+Programming Language ===> Python
+
+Description Of Files:
+
+heart_v1.py => For Run ===> python heart_v1.py
+
+Required packages For Python ===> turtle
+
+turtle ==> To install => pip install turtle
---
+++ heart_v1.py
@@ -0,0 +1,20 @@
+# Heart Design With Python - Version 1
+
+# Library
+import turtle
+
+# Design
+heart = turtle.Turtle()
+heart.color('red')
+heart.begin_fill()
+heart.left(140)
+heart.forward(180)
+heart.circle(-90, 200)
+heart.setheading(60)
+heart.circle(-90, 200)
+heart.forward(180)
+heart.end_fill()
+turtle.done()
+
+# Finish
+# Create By Moein Heshmati
